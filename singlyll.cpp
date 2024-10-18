c45e1164482720144fbc7b2201b02729698b8e0e
Goverdhan Kumar <58867508+wh04M1i@users.noreply.github.com>
2024-10-18 13:51:19 +0530

    Create singlyll.cpp

---
+++ singlyll.cpp
@@ -0,0 +1,4 @@
+Singly linked list or One way chain
+Singly linked list can be defined as the collection of ordered set of elements. The number of elements may vary according to need of the program. A node in the singly linked list consist of two parts: data part and link part. Data part of the node stores actual information that is to be represented by the node while the link part of the node stores the address of its immediate successor.
+
+One way chain or singly linked list can be traversed only in one direction. In other words, we can say that each node contains only next pointer, therefore we can not traverse the list in the reverse direction
